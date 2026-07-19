c83c30239d0e515c73f1fed093d1f538941f5d7e
reginpv <reginpv@gmail.com>
2026-07-19 23:00:53 +0800

    Update boilerplate vars to project name

--- config/constants.ts
+++ config/constants.ts
@@ -1,12 +1,12 @@
-export const APP_NAME = 'NextCrud'
+export const APP_NAME = 'SmileScore'
 export const APP_BASE_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://nextcrud.vercel.app'
+    ? 'https://smile-score-test.vercel.app'
     : 'http://localhost:3000'
 
-export const SCHOOL_NAME = 'NextCrud'
+export const SCHOOL_NAME = 'SmileScore'
 
-export const SMTP_FROM_NAME = 'NextCrud'
+export const SMTP_FROM_NAME = 'SmileScore'
 export const SMTP_FROM_EMAIL = 'hello@domain.com'
 
 export const USERS_PER_PAGE = 5
